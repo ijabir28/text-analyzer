6d82c297c131f08d6c165bd9d7adc8904587a654
Jabir <jabirikamal@gmail.com>
2025-01-12 23:36:18 +0600

    Implement word count logic in countOfWords function

--- domain/services/text-analyzer.js
+++ domain/services/text-analyzer.js
@@ -1,5 +1,9 @@
 function countOfWords(text) {
-    return 0;
+    if (text.trim() === "") {
+        return 0;
+    }
+
+    return text.trim().split(/\s+/).length;
 }
 
 module.exports = { countOfWords };
